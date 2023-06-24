7c824266af5d002a9204949f14a9f4e737a79b24
Dove <doveliu0516@gmail.com>
2023-06-24 16:52:02 +0800

    feat: change folder

---
+++ api/index.py
@@ -0,0 +1,1 @@
+from run.index import app
--- main.py
+++ main.py
@@ -1,63 +1,64 @@
-import os
-import uvicorn
-from fastapi import FastAPI, Request, HTTPException
+from api.index import app
+# import os
+# import uvicorn
+# from fastapi import FastAPI, Request, HTTPException
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-from config.config import Config, LineConfig
-from service.messageRead import messageRead
-# from spider.stickershopToExcel import stickers
+# from linebot import (
+#     LineBotApi, WebhookHandler
+# )
+# from linebot.exceptions import (
+#     InvalidSignatureError
+# )
+# from linebot.models import (
+#     MessageEvent, TextMessage, TextSendMessage,
+# )
+# from config.config import Config, LineConfig
+# from service.messageRead import messageRead
+# # from spider.stickershopToExcel import stickers
 
-# Line Bot config
-# your access token to line bot which get from line biz
-print('111')
-accessToken = LineConfig.ACCESS_TOKEN
-# your secret token to access line bot webhook get from line developer
-secret = LineConfig.SERCRET
-print('22')
+# # Line Bot config
+# # your access token to line bot which get from line biz
+# print('111')
+# accessToken = LineConfig.ACCESS_TOKEN
+# # your secret token to access line bot webhook get from line developer
+# secret = LineConfig.SERCRET
+# print('22')
 
-app = FastAPI()
+# app = FastAPI()
 
-line_bot_api = LineBotApi(accessToken)
-handler = WebhookHandler(secret)
+# line_bot_api = LineBotApi(accessToken)
+# handler = WebhookHandler(secret)
 
-@app.get("/")
-def root():
+# @app.get("/")
+# def root():
 
-    return{"hello":"world"}
+#     return{"hello":"world"}
 
-@app.post("/callback")
-async def echoBot(request: Request):
-    signature = request.headers["X-Line-Signature"]
-    body = await request.body()
-    try:
-        # 使用 handler 物件處理接收到的 Line Bot 事件。它接受解碼後的請求主體和簽名作為參數，並執行相應的處理邏輯。
-        handler.handle(body.decode(), signature)
-    except InvalidSignatureError:
-        raise HTTPException(status_code=400, detail="Missing Parameters")
-    return "OK"
-
-
-@handler.add(MessageEvent, message=(TextMessage))
-def handling_message(event):
-    replyToken = event.reply_token
-    print(event.message)
-    if isinstance(event.message, TextMessage):
-        # 使用者發的訊息
-        messages = event.message.text
-        messageRead.switch(event, messages)
-        # echoMessages = TextSendMessage(text=messages)
-        # line_bot_api.reply_message(
-        #     reply_token=replyToken, messages=echoMessages)
+# @app.post("/callback")
+# async def echoBot(request: Request):
+#     signature = request.headers["X-Line-Signature"]
+#     body = await request.body()
+#     try:
+#         # 使用 handler 物件處理接收到的 Line Bot 事件。它接受解碼後的請求主體和簽名作為參數，並執行相應的處理邏輯。
+#         handler.handle(body.decode(), signature)
+#     except InvalidSignatureError:
+#         raise HTTPException(status_code=400, detail="Missing Parameters")
+#     return "OK"
 
 
-# if __name__ == '__main__':
-#     server_port = int(os.environ.get('PORT', Config.PORT))
-#     uvicorn.run(app, host=Config.HOST, port=server_port, log_level="info")
+# @handler.add(MessageEvent, message=(TextMessage))
+# def handling_message(event):
+#     replyToken = event.reply_token
+#     print(event.message)
+#     if isinstance(event.message, TextMessage):
+#         # 使用者發的訊息
+#         messages = event.message.text
+#         messageRead.switch(event, messages)
+#         # echoMessages = TextSendMessage(text=messages)
+#         # line_bot_api.reply_message(
+#         #     reply_token=replyToken, messages=echoMessages)
+
+
+# # if __name__ == '__main__':
+# #     server_port = int(os.environ.get('PORT', Config.PORT))
+# #     uvicorn.run(app, host=Config.HOST, port=server_port, log_level="info")
